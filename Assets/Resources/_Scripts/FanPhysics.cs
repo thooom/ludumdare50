f65385803c99b384472cf36a225e5cfa4e10d1bf
tomerikhov <tom-erik.hovring@capgemini.com>
2022-04-02 19:59:48 +0200

    YeetScript

--- Assets/Resources/_Scripts/FanPhysics.cs
+++ Assets/Resources/_Scripts/FanPhysics.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FanPhysics : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector2 yeet;
+    float force;
+
+    public void Awake()
     {
-        
+        force = 5f;
+        yeet = transform.position - transform.parent.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(yeet * force * Time.deltaTime);
+            }
+        }
     }
 }
