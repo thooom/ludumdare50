67b73a54e3c012659b0c251566c32d173418abcc
tomerikhov <tom-erik.hovring@capgemini.com>
2022-04-02 16:59:24 +0200

    Vinden til viftene kan ikke lenger slettes :P

--- Assets/Resources/_Scripts/PlaceObjectLogic.cs
+++ Assets/Resources/_Scripts/PlaceObjectLogic.cs
@@ -31,7 +31,7 @@ public class PlaceObjectLogic : MonoBehaviour
         {
             Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
             RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, 100f);
-            if (hit.collider != null)
+            if (hit.collider != null && hit.collider.gameObject.tag == "Deletable")
             { 
                 Destroy(hit.collider.gameObject);
             }
