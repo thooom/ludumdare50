1f8f2608d384f53ceb86dc5f813af6dae9284a32
tomerikhov <tom-erik.hovring@capgemini.com>
2022-04-02 15:58:53 +0200

    Se hva spilleren kan rotere og plassere!

---
+++ Assets/Resources/_Scripts/PlaceObjectLogic.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceObjectLogic : MonoBehaviour
+{
+    private List<GameObject> objectsToPlace;
+    public GameObject lastPlacedObject;
+
+    void Awake()
+    {
+        objectsToPlace = new List<GameObject>();
+        objectsToPlace.Add(Resources.Load<GameObject>("Prefabs/Fan"));
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlaceObject();
+        }
+        if (lastPlacedObject != null && Input.GetMouseButton(0))
+        {
+            RotateObject();
+        }
+        if (lastPlacedObject != null && Input.GetMouseButtonUp(0))
+        {
+            lastPlacedObject = null;
+        }
+    }
+
+    private void PlaceObject()
+    {
+        var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0;
+        lastPlacedObject = Instantiate(objectsToPlace[GetActiveObject()], position, Quaternion.identity);
+    }
+
+    private void RotateObject()
+    {
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector2 mouseOnScreen = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+        lastPlacedObject.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+    }
+
+    private int GetActiveObject()
+    {
+        return 0;
+        // TODO: Connect med Inventory
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+}
