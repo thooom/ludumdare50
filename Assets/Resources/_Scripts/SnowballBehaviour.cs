5ef33471dfbd82959206038626ab974336f56514
Andreas Harnes <22345822+Andreas-Harnes@users.noreply.github.com>
2022-04-03 13:56:16 +0200

    snøball
    lagd prefab og lagt til scaling over tid

--- Assets/Resources/_Scripts/SnowballBehaviour.cs
+++ Assets/Resources/_Scripts/SnowballBehaviour.cs
@@ -2,20 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBehaviour : MonoBehaviour
+public class SnowballBehaviour : MonoBehaviour
 {
 
     public GameObject player;
-    private int scaling;
+
+    private Vector3 originalScale;
+    private Vector3 destinationScale;
+
+    public float healthLost;
+    public float startHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalScale = player.transform.localScale;
+        destinationScale = new Vector3(0.0f, 0.0f, 0.0f);
+
+        startHealth = 100.0f;
+        healthLost = 0.0f;
+
+        //StartCoroutine(ScaleOverTime(15));
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        player.transform.localScale = Vector3.Lerp(originalScale, destinationScale, healthLost / startHealth);
+        healthLost += 0.1f;
+
     }
+
 }
